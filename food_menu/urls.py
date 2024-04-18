39b417e4a0c15d7d996d5c4c1bb165a713909a84
Nikita <barskii.no@outlook.com>
2024-04-18 16:54:46 +0300

    Add index page

--- food_menu/urls.py
+++ food_menu/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from orderapp.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('orderapp/', include('orderapp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
