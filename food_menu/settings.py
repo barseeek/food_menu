b12130d823172ea375911e801660b6eeb46cfb9f
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-04-22 17:50:10 +0300

    add dj dayabase settings

--- food_menu/settings.py
+++ food_menu/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from environs import Env
+import dj_database_url
 
 env = Env()
 env.read_env()
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-()@l3h@x%7c^hbyp+iv8g-k@-zssbm-*_0y4)fawxo3+c$5-vi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 # Application definition
 
@@ -79,11 +80,18 @@ WSGI_APPLICATION = 'food_menu.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        default=env('DB_URL')
+    )
+}
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
